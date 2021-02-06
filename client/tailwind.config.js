c8ca4b5d0565265b87810b053377516d859bc11f
Satya Rohith <me@satyarohith.com>
2021-02-06 13:06:45 +0530

    chore(client): setup tailwindcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./kament.jsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
